300b535eb70e3cfe2fbb4021a7ece85c149cfb77
Ben Dilday <ben.dilday.phd@gmail.com>
2017-06-07 21:51:52 -0400

    cruft

--- server.R
+++ server.R
@@ -23,10 +23,6 @@ model_ranef <- list()
 print(.libPaths()[[1]])
 print(list.files(.libPaths()[[1]]))
 
-make_full_name <- function(df_data, df_master) {
-
-}
-
 for (year in c(2007, 2016)) {
   key <- as.character(year)
   cat(sprintf("loading data for year: %d\n", year))
@@ -60,28 +56,6 @@ shinyServer(function(input, output) {
     selectInput("pit_key", "select pit_id", pit_key)
   })
 
-  output$pit_list_label <- renderText({
-    pit_id_list <- unique(pit_ranef[[input$year]]$var_id)
-
-    tmp <- names(pit_ranef[[input$year]])
-    # for (i in seq_along(names(pit_ranef[[input$year]]))) {
-    #   v <- names(pit_ranef)[[i]]
-    #   tmp <- paste(v, tmp, sep=" ")
-    # }
-
-    # sprintf("year: %s nrow: %d %d %s ",
-    #         input$year,
-    #         length(pit_id_list),
-    #         nrow(pitcher_ranef_list[[input$year]]), tmp
-    #         )
-    #
-    sprintf("year: %s nrow: %d tmp %s ",
-            input$year,
-            length(pit_id_list), tmp
-    )
-
-  })
-
   output$distPlot <- renderPlot({
     fit_var <- "value"
     year = input$year
@@ -102,30 +76,7 @@ shinyServer(function(input, output) {
                    aes(x=model_name, y=fit_var, group=model_name), color='red', size=4) +
       theme(axis.text = element_text(face='bold', size=14),
             axis.title = element_text(size=22))
-
-
   })
-
-  output$model_label <- renderText({
-    year = input$year
-
-    # if (! year %in% names(pitcher_ranef_list)) {
-    #   pitcher_ranef_list[[year]] <- BProDRA::extract_pitcher_ranef(mods_list[[year]])
-    # }
-
-    tmp <- dra_runs[[input$year]]
-    tmp <- subset(tmp, pit_id == id_from_key(input$pit_key))
-    fit_df <- pit_ranef[[year]]
-#    sprintf("year: %s month; ", year)
- #   sprintf("year: %s nrow: %d pit_id: %s ", year, nrow(fit_df),input$pit_id)
-    sprintf("year: %s pit_id, %s nrow: %d %d %d %d %s ",
-            year, id_from_key(input$pit_key), nrow(fit_df),
-            nrow(dra_runs[[year]]),
-            nrow(pit_ranef[[year]]),
-            nrow(model_ranef[[year]]), summary(tmp))
-
-   sprintf("%s %d %s ", names(tmp), nrow(tmp), id_from_key(input$pit_key))
-    })
 
   output$pitcher_components <- renderTable({
     tmp <- dra_runs[[input$year]]
